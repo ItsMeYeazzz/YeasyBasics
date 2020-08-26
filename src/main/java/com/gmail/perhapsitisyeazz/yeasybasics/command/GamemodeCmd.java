43e59a957922c0ef90cf0f72b5285451bd92eecc
YeaZz <perhapsitisyeazz@gmail.com>
2020-08-26 22:59:26 +0200

    Just an update bro

--- src/main/java/com/gmail/perhapsitisyeazz/yeasybasics/command/GamemodeCmd.java
+++ src/main/java/com/gmail/perhapsitisyeazz/yeasybasics/command/GamemodeCmd.java
@@ -1,10 +1,13 @@
 package com.gmail.perhapsitisyeazz.yeasybasics.command;
 
 import com.gmail.perhapsitisyeazz.yeasybasics.YeasyBasics;
+import net.md_5.bungee.api.chat.ClickEvent;
 import net.md_5.bungee.api.chat.ComponentBuilder;
 import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.chat.HoverEvent;
 import net.md_5.bungee.api.chat.TextComponent;
+import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -31,13 +34,11 @@ public class GamemodeCmd implements CommandExecutor {
             "Spectator gamemode"
     );
 
+    @SuppressWarnings("deprecation")
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
         if (sender instanceof Player) {
             Player player = (Player) sender;
-            if (!player.hasPermission("gamemode")) {
-                sender.sendMessage(ChatColor.RED + "You do not have the permission.");
-            }
             if (args.length == 0) {
                 ComponentBuilder builder = new ComponentBuilder();
                 builder
@@ -45,23 +46,51 @@ public class GamemodeCmd implements CommandExecutor {
                         .append("Basics").color(ChatColor.DARK_AQUA)
                         .append("]").color(ChatColor.DARK_GRAY)
                         .append(" Correct usage:").color(ChatColor.DARK_GREEN);
-                for(String sub : subCmd) {
-                    for(String desc : descCmd) {
+                for (String sub : subCmd) {
+                    for (String desc : descCmd) {
                         String arg = sub.split(" ")[0].substring(0, 1).toUpperCase() + sub.split(" ")[0].substring(1);
                         builder
                                 .append("\n» ").color(ChatColor.DARK_AQUA)
                                 .append("/gamemode " + sub).color(ChatColor.GREEN)
-                                .event(new HoverEvent.Action(HoverEvent.Action.SHOW_TEXT, TextComponent.fromLegacyText(
-                                        ChatColor.DARK_GREEN + "Command: " + ChatColor.AQUA + arg +
+                                .event(new HoverEvent(HoverEvent.Action.SHOW_TEXT, TextComponent.fromLegacyText(
+                                                ChatColor.DARK_GREEN + "Command: " + ChatColor.AQUA + arg +
                                                 ChatColor.DARK_GREEN + "\nDescription: " + ChatColor.AQUA + desc +
-                                                ChatColor.DARK_GREEN + "\nUsage: " + ChatColor.AQUA + "gamemode " + sub +
-                                                "\n\n" + ChatColor.GRAY + "Click to execute.")));
+                                                ChatColor.DARK_GREEN + "\nUsage: " + ChatColor.AQUA + command.getName() + "" + sub +
+                                                "\n\n" + ChatColor.GRAY + "Click to execute.")))
+                                .event(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "gamemode " + sub));
                     }
                 }
             }
+            Player playerArg = Bukkit.getPlayer(args[2]);
+            setGamemode(args[0], playerArg != null ? playerArg : player);
         } else {
             sender.sendMessage(ChatColor.RED + "Player-only commands.");
         }
         return true;
+    }
+
+    private void setGamemode(String arg, Player player) {
+        GameMode gm;
+        if(match(arg, "survival", "s", "0")) gm = GameMode.SURVIVAL;
+        else if(match(arg, "creative", "c", "1")) gm = GameMode.CREATIVE;
+        else if(match(arg, "adventure", "a", "2")) gm = GameMode.ADVENTURE;
+        else if(match(arg, "spectator", "s", "3")) gm = GameMode.SPECTATOR;
+        else {
+            player.sendActionBar(ChatColor.RED + "Error : Argument 2");
+            return;
+        }
+        if(gm == player.getGameMode()) {
+            player.sendActionBar(ChatColor.DARK_GREEN + "You are already in " + gm.name() + ".");
+        } else {
+            player.setGameMode(gm);
+            player.sendActionBar(ChatColor.GREEN + "Your gamemode has been set to survival " + gm.name() + ".");
+        }
+    }
+
+    private boolean match(String arg, String... str){
+        for(String m : str){
+            if(m.equalsIgnoreCase(arg)) return true;
+        }
+        return false;
     }
 }
