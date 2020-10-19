089d664762f48999ebe9d957f6c0efaa576f5949
YeaZz <perhapsitisyeazz@gmail.com>
2020-10-19 02:32:32 +0200

    Just an update bro

---
+++ src/main/java/com/gmail/perhapsitisyeazz/yeasybasics/command/InvSeeCmd.java
@@ -0,0 +1,25 @@
+package com.gmail.perhapsitisyeazz.yeasybasics.command;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+public class InvSeeCmd implements CommandExecutor {
+
+	@Override
+	public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+		if(args.length > 0) {
+			if (sender instanceof Player) {
+				Player player = (Player) sender;
+				Player target = Bukkit.getPlayer(args[0]);
+				if(target == null) return true;
+				player.openInventory(target.getInventory());
+				player.sendActionBar("yes");
+			}
+		}
+		return true;
+	}
+}
--- src/main/java/com/gmail/perhapsitisyeazz/yeasybasics/listener/UtilsEvt.java
+++ src/main/java/com/gmail/perhapsitisyeazz/yeasybasics/listener/UtilsEvt.java
@@ -1,7 +1,11 @@
 package com.gmail.perhapsitisyeazz.yeasybasics.listener;
 
+import com.gmail.perhapsitisyeazz.yeasybasics.manager.Message;
 import com.gmail.perhapsitisyeazz.yeasybasics.util.Utils;
 import net.md_5.bungee.api.ChatColor;
+import org.bukkit.Bukkit;
+import org.bukkit.Sound;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
@@ -11,6 +15,7 @@ import org.bukkit.event.player.PlayerQuitEvent;
 public class UtilsEvt implements Listener {
 
     private final Utils utils = new Utils();
+    private final Message message = new Message();
 
     @EventHandler
     private void onJoin(PlayerJoinEvent event) {
@@ -28,5 +33,12 @@ public class UtilsEvt implements Listener {
     @EventHandler
     private void onChat(AsyncPlayerChatEvent event) {
         event.setMessage(ChatColor.translateAlternateColorCodes('&', event.getMessage()));
+        for(Player player : Bukkit.getOnlinePlayers()) {
+            if(player == event.getPlayer()) continue;
+            if(event.getMessage().contains(player.getName())) {
+                player.sendActionBar(message.logo + utils.getColMsg("&b" + event.getPlayer() + " &7has pinged you in the chat."));
+                player.playSound(player.getLocation(), Sound.BLOCK_NOTE_BLOCK_PLING, 0.1F, 1F);
+            }
+        }
     }
 }
