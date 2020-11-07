a6fa0becee9647d0876302ce1a0f52bce4a16898
YeaZz <perhapsitisyeazz@gmail.com>
2020-11-07 16:59:26 +0100

    Just an update bro

--- src/main/java/com/gmail/perhapsitisyeazz/yeasybasics/listeners/CastSpellEvent.java
+++ src/main/java/com/gmail/perhapsitisyeazz/yeasybasics/listeners/CastSpellEvent.java
@@ -15,6 +15,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
+import org.bukkit.scheduler.BukkitRunnable;
 
 import java.util.ArrayList;
 
@@ -29,52 +30,112 @@ public class CastSpellEvent implements Listener {
 
 	@EventHandler
 	private void onCastSpell(SpellCastEvent event) {
+		YeasyBasics instance = new YeasyBasics();
 		Player player = event.getPlayer();
 		Spell spell = event.getSpell();
 		SpellType type = spell.getType();
 		int level = spell.getLevel();
 		Location location = player.getLocation();
+		player.sendActionBar(SM.sendSpellMessage(spell));
 		if (type == SpellType.BUNNY_HOP) {
-			player.sendActionBar(SM.sendSpellMessage(spell));
 			int duration = level * 5;
 			int amplifier = level - 1;
 			PotionEffect effect = new PotionEffect(PotionEffectType.JUMP, duration, amplifier, false, false, false);
 			player.addPotionEffect(effect);
 			player.playSound(location, Sound.ENTITY_RABBIT_HURT, 1, 1);
 		} else if(type == SpellType.BUBBLE) {
-
+			player.playSound(location, Sound.BLOCK_BUBBLE_COLUMN_BUBBLE_POP, 1, 1);
 		} else if(type == SpellType.GIFT_OF_RAGE) {
-			player.sendActionBar(SM.sendSpellMessage(spell));
 			int duration = level * 4;
 			int amplifier = level - 1;
 			PotionEffect effect = new PotionEffect(PotionEffectType.INCREASE_DAMAGE, duration, amplifier, false, false, false);
 			player.addPotionEffect(effect);
 			player.playSound(location, Sound.ENTITY_ENDER_DRAGON_GROWL, 1, 1);
 		} else if(type == SpellType.ROYAL_DINNER) {
-			player.sendActionBar(SM.sendSpellMessage(spell));
-			for(Player loopPlayer : getNearbyPlayers(player, level*6)) {
+			for(Player loopPlayer : getNearbyPlayers(player, 8)) {
 				loopPlayer.sendActionBar(Util.getColMsg("&2You have been feed by &b" + player.getName()));
-				loopPlayer.setFoodLevel(20);
-				if(level == spell.getMaxLevel()) loopPlayer.setSaturation(20);
+				int food = loopPlayer.getFoodLevel() + 5 * (level + 1);
+				loopPlayer.setFoodLevel(food);
+				if(level == spell.getMaxLevel())
+					loopPlayer.setSaturation(food);
+				for(int i = 0; i < 3; i++) {
+					BukkitRunnable runnable;
+					runnable = new BukkitRunnable() {
+						@Override
+						public void run() {
+							player.playSound(location, Sound.ENTITY_GENERIC_EAT, 1, 1);
+						}
+					};
+					runnable.runTaskLater(instance, 5*i);
+				}
+			}
+		} else if(type == SpellType.VITAL_DISCHARGE) {
+			ArrayList<Player> nearbyPlayers = getNearbyPlayers(player, 8, 12);
+			int size = nearbyPlayers.size();
+			for(Player loopPlayer : nearbyPlayers) {
+				loopPlayer.sendActionBar(Util.getColMsg("&2You have been heal by &b" + player.getName()));
+				double health = loopPlayer.getHealth() + 5 * level * (1 - (size > 2 ? (size-2)/10F : 0));
+				loopPlayer.setHealth(health);
+			}
+			player.playSound(location, Sound.ENTITY_SPLASH_POTION_BREAK, 1, 1);
+		} else if(type == SpellType.SOLAR_GLOW) {
+			player.playSound(location, Sound.ENTITY_LIGHTNING_BOLT_THUNDER, 3, 1);
+			ArrayList<Entity> nearbyMonsters = getNearbyMonsters(player, 15, 15);
+			for(int i = 0; i < 2; i++) {
+				BukkitRunnable runnable;
+				runnable = new BukkitRunnable() {
+					@Override
+					public void run() {
+						for (Entity loopMonster : nearbyMonsters) {
+							player.getWorld().strikeLightningEffect(loopMonster.getLocation());
+						}
+					}
+				};
+				runnable.runTaskLater(instance, 30 + i*5);
 			}
 		}
 	}
 
-	public ArrayList<Entity> getNearbyMonsters(Player player, double range){
+	public ArrayList<Entity> getNearbyMonsters(Player player, double range) {
 		ArrayList<Entity> nearby = new ArrayList<>();
-		for (Entity e : player.getNearbyEntities(range, range, range)){
-			if (e instanceof Monster){
+		for(Entity e : player.getNearbyEntities(range, range, range)){
+			if(e instanceof Monster){
 				nearby.add(e);
 			}
 		}
 		return nearby;
 	}
 
-	public ArrayList<Player> getNearbyPlayers(Player player, double range){
+	public ArrayList<Entity> getNearbyMonsters(Player player, double range, int limit) {
+		ArrayList<Entity> nearby = new ArrayList<>();
+		for(Entity e : player.getNearbyEntities(range, range, range)){
+			if(e instanceof Monster){
+				if(nearby.size() < limit)
+					nearby.add(e);
+				else
+					break;
+			}
+		}
+		return nearby;
+	}
+
+	public ArrayList<Player> getNearbyPlayers(Player player, double range) {
 		ArrayList<Player> nearby = new ArrayList<>();
-		for (Entity e : player.getNearbyEntities(range, range, range)){
-			if (e instanceof Player){
+		for(Entity e : player.getNearbyEntities(range, range, range)) {
+			if(e instanceof Player)
 				nearby.add((Player) e);
+		}
+		return nearby;
+	}
+
+	public ArrayList<Player> getNearbyPlayers(Player player, double range, int limit) {
+		ArrayList<Player> nearby = new ArrayList<>();
+		for(Entity e : player.getNearbyEntities(range, range, range)){
+			if(e instanceof Player){
+				if(nearby.size() < limit)
+					nearby.add((Player) e);
+				else
+					break;
 			}
 		}
 		return nearby;
