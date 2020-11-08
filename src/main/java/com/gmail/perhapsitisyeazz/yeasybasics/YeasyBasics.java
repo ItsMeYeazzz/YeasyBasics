c716101c53d717799b4ffc50a7a938846edfa46b
YeaZz <perhapsitisyeazz@gmail.com>
2020-11-08 12:04:05 +0100

    Just an update bro

--- src/main/java/com/gmail/perhapsitisyeazz/yeasybasics/YeasyBasics.java
+++ src/main/java/com/gmail/perhapsitisyeazz/yeasybasics/YeasyBasics.java
@@ -5,8 +5,6 @@ import com.gmail.perhapsitisyeazz.yeasybasics.listeners.CastSpellEvent;
 import com.gmail.perhapsitisyeazz.yeasybasics.listeners.UtilsEvt;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.io.File;
-
 public class YeasyBasics extends JavaPlugin {
 
     private static YeasyBasics instance;
@@ -15,15 +13,9 @@ public class YeasyBasics extends JavaPlugin {
         return instance;
     }
 
-    public final File spellFile = new File("plugins/YeasyBasics/spells/");
-
     @Override
     public void onEnable() {
         instance = this;
-        if(!spellFile.exists()) {
-            boolean wasCreated = spellFile.mkdirs();
-            getLogger().warning("Directory creation " + (wasCreated ? "successful." : "failed."));
-        }
         this.getServer().getPluginManager().registerEvents(new UtilsEvt(this), this);
         this.getServer().getPluginManager().registerEvents(new CastSpellEvent(this), this);
         this.getCommand("gamemode").setExecutor(new GamemodeCmd());
